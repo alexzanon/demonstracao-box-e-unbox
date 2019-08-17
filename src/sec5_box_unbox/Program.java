f48b4af78682d864f6cc5560ecd460c7ed5acb50
Alex Zanon <alexzanonvl@hotmail.com>
2019-08-17 11:22:35 -0300

    Programa para demonstração de box e unbox

---
+++ src/sec5_box_unbox/Program.java
@@ -0,0 +1,19 @@
+package sec5_box_unbox;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int x =20;
+		
+		Object obj = x;
+		
+		System.out.println(obj);
+		
+		int y = (int) obj;
+		
+		System.out.println(y);
+
+	}
+
+}
